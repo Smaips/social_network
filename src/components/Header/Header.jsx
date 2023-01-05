22560542afa757bae6fe328ab89161e45f347de0
Smaips <mihailsvistun@gmail.com>
2023-01-05 15:48:19 +0100

    BrowserRouter setting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css'
 const Header = () => {
     return (
         <header className={s.header}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdswHMmofGndK8c4cIHsYs32fs7vCUCemBww&usqp=CAU"></img>
+          <img alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdswHMmofGndK8c4cIHsYs32fs7vCUCemBww&usqp=CAU"></img>
         </header>
     )
 }
