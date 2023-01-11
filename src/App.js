7e6ff55574724523f38a2fc4b7b43fb919ce8a00
Smaips <mihailsvistun@gmail.com>
2023-01-11 16:34:54 +0100

    Moved BrowserRouter from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App(props) {
   
   return (
-    <HashRouter>
         <div className='app-wrapper'>
           <Header />
           <Navbar />
@@ -22,7 +21,6 @@ function App(props) {
             </Routes>
           </div>
         </div>
-     </HashRouter >
   );
 }
 
