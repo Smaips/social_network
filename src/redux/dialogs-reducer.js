6f2f23539d1390df6afdf2c72ee976a48493ec7f
Smaips <mihailsvistun@gmail.com>
2023-01-29 23:51:08 +0100

    added reducer

---
+++ src/redux/dialogs-reducer.js
@@ -0,0 +1,21 @@
+const SEND_MESSAGE = 'SEND-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
+const dialogReducer = (state, action) => {
+    if (action.type === SEND_MESSAGE) {
+        let newMessage = {
+            id: 5,
+            message: state.newMessageText,
+        };
+
+        state.messages.push(newMessage);
+        state.newMessageText = '';
+    }
+    else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+        state.newMessageText = action.newText;
+    }
+
+    return state;
+}
+
+export default dialogReducer; 
